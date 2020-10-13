efaabff362dc61d2f8bb92fb86d0def5942328a8
carlos <cfabela@itexico.com>
2020-10-13 14:44:33 -0500

    -A  Adding UWP project

---
+++ Test.UWP/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Windows.Foundation;
+using Windows.UI.ViewManagement;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Test.UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.LoadApplication(new Test.App());
+        }
+    }
+}
