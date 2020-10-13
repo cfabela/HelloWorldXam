2552acaf63dd79ecd9191ab0b7a48b8665d57579
Carlos Fabela <cfabela@itexico.com>
2020-10-13 11:01:56 -0500

    no message

--- Test.macOS/AppDelegate.cs
+++ Test.macOS/AppDelegate.cs
@@ -1,23 +1,36 @@
 ﻿using AppKit;
 using Foundation;
 
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
 namespace Test.macOS
 {
     [Register("AppDelegate")]
-    public class AppDelegate : NSApplicationDelegate
+    public class AppDelegate : FormsApplicationDelegate
     {
+
+        NSWindow _window;
+
         public AppDelegate()
         {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(200, 200, 800, 600);
+            _window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            _window.Title = "Mind of Ai";
         }
 
-        public override void DidFinishLaunching(NSNotification notification)
-        {
-            // Insert code here to initialize your application
-        }
+        public override NSWindow MainWindow => _window;
 
-        public override void WillTerminate(NSNotification notification)
+
+
+        [Export("applicationDidFinishLaunching:")]
+        public void DidFinishLaunching(NSNotification notification)
         {
-            // Insert code here to tear down your application
+            Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
         }
     }
 }
